33db9f5dfb64e1ed7c4483ca46cad2cb3080389d
Joseph Mould <mould_joe@hotmail.com>
2024-09-12 10:17:44 +0100

    refactor: add sensible return statements and pretty printing / write of json

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import datetime
 import json
 import os
+import pprint
 
 
 JSON_FILENAME = "tasks.json"
@@ -16,7 +17,7 @@ def read_json(json_filepath):
 
 def write_json(json_filepath, json_data):
     with open(json_filepath, "w") as json_fp:
-        json.dump(json_data, json_fp)
+        json.dump(json_data, json_fp, indent=4)
 
 
 def create_tasks_json(json_filepath):
@@ -34,14 +35,16 @@ def add_task(json_filepath, description):
     created_timestamp = datetime.datetime.now().isoformat()
     tasks_json["metadata"]["num_tasks"] += 1
     # json writes dict keys as str
-    tasks_json["tasks"][str(_id)] = {
+    task = {
         "id": _id,
         "description": description,
         "status": "todo",
         "createdAt": created_timestamp,
         "updatedAt": created_timestamp,
     }
+    tasks_json["tasks"][str(_id)] = task
     write_json(json_filepath, tasks_json)
+    return task
 
 
 def update_task(json_filepath, _id, description):
@@ -50,12 +53,14 @@ def update_task(json_filepath, _id, description):
     task["description"] = description
     task["updatedAt"] = get_iso_datetime()
     write_json(json_filepath, tasks_json)
+    return task
 
 
 def delete_task(json_filepath, _id):
     tasks_json = read_json(json_filepath)
     del tasks_json["tasks"][str(_id)]
     write_json(json_filepath, tasks_json)
+    return f"Task of id {_id} deleted from {JSON_FILENAME}."
 
 
 def mark_task_status(json_filepath, _id, status):
@@ -64,6 +69,7 @@ def mark_task_status(json_filepath, _id, status):
     task["status"] = status
     task["updatedAt"] = get_iso_datetime()
     write_json(json_filepath, tasks_json)
+    return task
 
 
 def list_tasks(json_filepath, status):
@@ -75,38 +81,36 @@ def list_tasks(json_filepath, status):
 
 
 def main():
-    # create the json file if doesn't exist
     if not os.path.isfile(JSON_FILEPATH):
         create_tasks_json(JSON_FILEPATH)
 
-    # parse cli args
     parser = argparse.ArgumentParser(
         prog="task-cli", description="To-do list cli application"
     )
     parser.set_defaults(which=None)
     subparsers = parser.add_subparsers()
-    # add
+
     task_adder = subparsers.add_parser("add")
     task_adder.add_argument("description", type=str)
     task_adder.set_defaults(which="add")
-    # update
+
     task_updater = subparsers.add_parser("update")
     task_updater.add_argument("id", type=int)
     task_updater.add_argument("description", type=str)
     task_updater.set_defaults(which="update")
-    # delete
+
     task_deleter = subparsers.add_parser("delete")
     task_deleter.add_argument("id", type=int)
     task_deleter.set_defaults(which="delete")
-    # mark-in-progress
+
     task_mark_in_progress = subparsers.add_parser("mark-in-progress")
     task_mark_in_progress.add_argument("id", type=int)
     task_mark_in_progress.set_defaults(which="mark-in-progress")
-    # mark-done
+
     task_mark_done = subparsers.add_parser("mark-done")
     task_mark_done.add_argument("id", type=int)
     task_mark_done.set_defaults(which="mark-done")
-    # list
+
     task_lister = subparsers.add_parser("list")
     task_lister.add_argument("status", nargs="?", type=str)
     task_lister.set_defaults(which="list")
@@ -115,21 +119,19 @@ def main():
 
     match args.which:
         case "add":
-            return add_task(JSON_FILEPATH, args.description)
+            pprint.pprint(add_task(JSON_FILEPATH, args.description))
         case "update":
-            return update_task(JSON_FILEPATH, args.id, args.description)
+            pprint.pprint(update_task(JSON_FILEPATH, args.id, args.description))
         case "delete":
-            return delete_task(JSON_FILEPATH, args.id)
+            print(delete_task(JSON_FILEPATH, args.id))
         case "mark-in-progress":
-            return mark_task_status(JSON_FILEPATH, args.id, "in-progress")
+            pprint.pprint(mark_task_status(JSON_FILEPATH, args.id, "in-progress"))
         case "mark-done":
-            return mark_task_status(JSON_FILEPATH, args.id, "done")
+            pprint.pprint(mark_task_status(JSON_FILEPATH, args.id, "done"))
         case "list":
-            return list_tasks(JSON_FILEPATH, args.status)
+            pprint.pprint(list_tasks(JSON_FILEPATH, args.status))
         case None:
             parser.print_help()
-            return
-    return
 
 
 if __name__ == "__main__":
