7ff25677c6ee725f0398e7623a388b8534d1d6b8
Joseph Mould <mould_joe@hotmail.com>
2024-09-22 11:51:39 +0100

    typing: adds type checking with mypy and docstrings

--- src/task_tracker/tasks.py
+++ src/task_tracker/tasks.py
@@ -1,16 +1,38 @@
+from __future__ import annotations
+
 import os
+from typing import TypedDict
 
 import task_tracker.utils as utils
 
 
+Metadata = dict[str, int]
+TaskDict = TypedDict(
+    "TaskDict",
+    {"id": int, "description": str, "status": str, "createdAt": str, "updatedAt": str},
+)
+Tasks = dict[str, TaskDict]
+TasksJson = TypedDict("TasksJson", {"metadata": Metadata, "tasks": Tasks}, total=False)
+
+
 class Task:
+    """Class representing a single task.
+
+    Attributes:
+        id: Unique integer id.
+        description: Task description string.
+        status: Task status string.
+        createdAt: Isoformat timestamp string at task creation.
+        updatedAt: Isoformat timestamp string at task last update.
+    """
+
     def __init__(
         self,
-        _id,
-        description,
-        status="todo",
-        created_at=utils.get_iso_datetime(),
-        updated_at=utils.get_iso_datetime(),
+        _id: int,
+        description: str,
+        status: str = "todo",
+        created_at: str = utils.get_iso_datetime(),
+        updated_at: str = utils.get_iso_datetime(),
     ):
         self.id = _id
         self.description = description
@@ -19,20 +41,20 @@ class Task:
         self.updated_at = updated_at
 
     @classmethod
-    def from_dict(cls, task_dict):
+    def from_dict(cls, task_dict: TaskDict) -> Task:
         return cls(
             task_dict["id"],
-            task_dict.get("description"),
-            task_dict.get("status"),
-            task_dict.get("createdAt"),
-            task_dict.get("updatedAt"),
+            task_dict["description"],
+            task_dict["status"],
+            task_dict["createdAt"],
+            task_dict["updatedAt"],
         )
 
     def update(self, update_field, update_value):
         setattr(self, update_field, update_value)
         self.updated_at = utils.get_iso_datetime()
 
-    def to_dict(self):
+    def to_dict(self) -> TaskDict:
         return {
             "id": self.id,
             "description": self.description,
@@ -43,20 +65,28 @@ class Task:
 
 
 class TasksFile:
-    def __init__(self, filepath):
+    """Class for reading / modifying the task json file.
+
+    Attributes:
+        filepath: Filepath string to tasks json file.
+        metadata: Metadata dict related to task operations.
+        tasks: Tasks mapping containing individual task dicts.
+    """
+
+    def __init__(self, filepath: str):
         self.filepath = filepath
-        self.filename = os.path.basename(filepath)
         if not os.path.isfile(filepath):
-            tasks_obj = {}
+            tasks_json: TasksJson = {}
         else:
-            tasks_obj = utils.read_json(filepath)
-        self.metadata = tasks_obj.get("metadata", {})
-        self.tasks = tasks_obj.get("tasks", {})
+            tasks_json = utils.read_json(filepath)
+        self.metadata: Metadata = tasks_json.get("metadata", {})
+        self.tasks: Tasks = tasks_json.get("tasks", {})
 
     def _get_task_by_id(self, _id):
         return Task.from_dict(self.tasks[str(_id)])
 
-    def add_task(self, description):
+    def add_task(self, description: str) -> TaskDict:
+        """Add a task to the tasks file."""
         task_id = self.metadata.get("task_counter", 0) + 1
         task_dict = Task(task_id, description).to_dict()
         self.tasks[str(task_id)] = task_dict
@@ -72,18 +102,22 @@ class TasksFile:
         self._save()
         return updated_task_dict
 
-    def update_task_description(self, _id, description):
+    def update_task_description(self, _id: int, description: str) -> TaskDict:
+        """Update a tasks description in the tasks file."""
         return self._update_task(_id, "description", description)
 
-    def update_task_status(self, _id, status):
+    def update_task_status(self, _id: int, status: str) -> TaskDict:
+        """Update a tasks status in the tasks file."""
         return self._update_task(_id, "status", status)
 
-    def delete_task(self, _id):
+    def delete_task(self, _id: int) -> str:
+        """Delete a task from the task file."""
         del self.tasks[str(_id)]
         self._save()
         return f"Task deleted successfully (ID: {_id})"
 
-    def list_tasks_by_status(self, status):
+    def list_tasks_by_status(self, status: str | None) -> list[TaskDict]:
+        """List tasks in the task file by status or with falsy status for all."""
         return [
             task
             for task in self.tasks.values()
--- src/task_tracker/utils.py
+++ src/task_tracker/utils.py
@@ -1,22 +1,29 @@
 import datetime
 import json
 
+from typing import TYPE_CHECKING
 
-def read_json(json_filepath):
+if TYPE_CHECKING:
+    from task_tracker.tasks import TasksJson
+
+
+def read_json(
+    json_filepath: str,
+) -> "TasksJson":
     with open(json_filepath) as json_fp:
-        _json = json.load(json_fp)
-    return _json
+        json_data = json.load(json_fp)
+    return json_data
 
 
-def write_json(json_filepath, json_data):
+def write_json(json_filepath: str, json_data) -> None:
     with open(json_filepath, "w") as json_fp:
         json.dump(json_data, json_fp, indent=4)
 
 
-def create_tasks_json(json_filepath):
+def create_tasks_json(json_filepath: str) -> None:
     tasks_json = {"metadata": {"num_tasks": 0}, "tasks": {}}
     write_json(json_filepath, tasks_json)
 
 
-def get_iso_datetime():
+def get_iso_datetime() -> str:
     return datetime.datetime.now().isoformat()
